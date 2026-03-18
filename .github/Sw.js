93fc980fcd6adb3c45ba626981cbc434fe43a29c
Domii87 <bolf90@gmail.com>
2026-03-18 18:23:20 +0100

    Add service worker for caching resources
    Implement a service worker for caching and offline support.

---
+++ .github/Sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'gold-studio-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+// Installieren des Service Workers und Speichern der Dateien im Cache
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Dateien aus dem Cache laden, wenn keine Internetverbindung besteht
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Alte Caches löschen, wenn eine neue Version verfügbar ist
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
